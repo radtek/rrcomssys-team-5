57cb4f6cacf845172e104b7f8b869dd6a1304083
jeanrodriguez24 <jeanrodriguez24@e25b1dc4-2701-11de-895d-8b024f6b28d7>
2009-04-19 18:34:37 +0000

    Synthesis Engine skeleton

---
+++ RRComSSys/RRComSSys.SynthesisEngine/CommandPattern.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.Specialized;
+
+namespace RRComSSys.SynthesisEngine
+{
+    public class Command
+    {
+        delegate string Invoker(StringCollection parameters);
+        static Invoker VoiceCall; //Add more commands here
+        
+        public Command(SkypeReceiver receiver)
+        {
+            VoiceCall = receiver.MakeAVoiceCall;
+        }        
+    }
+}
---
+++ RRComSSys/RRComSSys.SynthesisEngine/SEClient.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RRComSSys.SynthesisEngine
+{
+    public class SEClient
+    {
+        private Command _command;
+        public SEClient()
+        {
+            _command = new Command(new SkypeReceiver());
+        }
+
+        public Command MyCommand
+        {
+            get
+            {
+                return _command;
+            }
+        }
+    }
+}
