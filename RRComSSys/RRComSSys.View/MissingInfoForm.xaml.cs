b429bd3238a261c6c59a3ec270a3db92304169a0
jeanrodriguez24 <jeanrodriguez24@e25b1dc4-2701-11de-895d-8b024f6b28d7>
2009-04-19 18:04:23 +0000

    UI is done for the most part

---
+++ RRComSSys/RRComSSys.View/MissingInfoForm.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Navigation;
+
+namespace RRComSSys.View
+{
+	public partial class MissingInfoForm
+	{
+		public MissingInfoForm()
+		{
+			this.InitializeComponent();
+			
+			// Insert code required on object creation below this point.
+		}
+	}
+}
